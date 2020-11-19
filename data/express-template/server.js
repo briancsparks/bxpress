4ae4cfcf46c14a31960c9630694ee61897948780
Brian C Sparks <briancsparks@gmail.com>
2020-11-18 18:17:31 -0800

    first working

--- data/express-template/server.js
+++ data/express-template/server.js
@@ -1,34 +1,90 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express       = require('express');
+const morgan        = require('morgan');
+const cors          = require('cors');
+const createError   = require('http-errors');
+const path          = require('path');
+const cookieParser  = require('cookie-parser');
+// const bodyParser    = require('body-parser');      // npm i body-parser is popular, too
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app           = express();
 
-var app = express();
+// Reads `.env` file and adds sets on process.env
+require('dotenv').config();
 
+// -------------------------------------------------------------------------------------------------------------------
+// import API routes
+const apiAuthRoutes       = require('./routes/api/apiAuthRoutes');
+const apiUserRoutes       = require('./routes/api/apiUserRoutes');
+
+// import routes
+const indexRoutes         = require('./routes/indexRoutes');      // Left over from generate-express
+const loginRoutes         = require('./routes/loginRoutes');
+const registrationRoutes  = require('./routes/registrationRoutes');
+
+// -------------------------------------------------------------------------------------------------------------------
+// ENV
+const port          = process.env.PORT          || 8000;
+const clientPort    = process.env.CLIENT_PORT   || 3003;
+
+// -------------------------------------------------------------------------------------------------------------------
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+// -------------------------------------------------------------------------------------------------------------------
+// Middlewares
+
+app.use(morgan('dev'));
 app.use(express.json());
+// app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use(cors()); // allows all origins
+if ((process.env.NODE_ENV = 'development')) {
+  // TODO: dont full path
+  app.use(cors({ origin: `http://localhost:${clientPort}` }));
+}
+
+
+// -------------------------------------------------------------------------------------------------------------------
+// API Routes
+app.use('/api',         apiAuthRoutes);
+app.use('/api',         apiUserRoutes);
+
+// Routes
+app.use('/',            indexRoutes);
+app.use("/login",       loginRoutes);
+app.use("/register",    registrationRoutes);
+
+// -------------------------------------------------------------------------------------------------------------------
+// Error handling
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(_404);
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(errorHandler);
+
+// -------------------------------------------------------------------------------------------------------------------
+// Start
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}: / and /api`);
+});
+
+
+// -------------------------------------------------------------------------------------------------------------------
+// functions
+
+function _404(req, res, next) {
+  next(createError(404));
+}
+
+function errorHandler(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,6 +92,4 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-module.exports = app;
+}
